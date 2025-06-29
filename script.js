4fa52e5ebc92ebcb15c8907147473ce97cfb517d
jazzzg <jazzhang4108@gmail.com>
2025-06-28 22:06:56 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-
+document.body.style.backgroundColor = "#DFDBE5";
+document.body.style.backgroundImage = `
+  linear-gradient(to right, rgba(156, 146, 172, 0.4) 1px, transparent 1px),
+  linear-gradient(to bottom, rgba(156, 146, 172, 0.4) 1px, transparent 1px)
+`;
+document.body.style.backgroundSize = "20px 20px";
